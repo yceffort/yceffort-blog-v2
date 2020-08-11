bca4f9a1d300d8f9acabb8cb9e047b48111d6717
yceffort <yceffort@gmail.com>
2020-08-11 18:42:11 +0900

    fix: uninstall katex

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-139493546-1',
   useKatex: false,
   menu: [
     {

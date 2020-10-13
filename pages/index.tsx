6c78c8f5ab518d04a9a063ee4a063df29e19c575
yceffort <yceffort@gmail.com>
2020-10-13 15:06:41 +0900

    [#80] 인덱스 페이지 완성

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,33 +4,32 @@ import { GetStaticProps } from 'next'
 import config from '../config'
 import Layout from '../src/components/Layout/Layout'
 import { DEFAULT_NUMBER_OF_POSTS } from '../src/types/const'
-import { FrontMatter } from '../src/types/types'
-import { getAllFrontMatters } from '../src/utils/frontMatters'
+import { Post } from '../src/types/types'
+import { getAllPosts } from '../src/utils/frontMatters'
 import Sidebar from '../src/components/Sidebar/Sidebar'
 import Page from '../src/components/Page/Page'
+import Feed from '../src/components/Feed/Feed'
+import Pagination from '../src/components/Pagination/Pagination'
 
-export default function Index({
-  recentPosts,
-}: {
-  recentPosts: Array<FrontMatter>
-}) {
+export default function Index({ recentPosts }: { recentPosts: Array<Post> }) {
   return (
     <Layout title={config.title} description={config.subtitle}>
       <Sidebar />
       <Page>
-        {recentPosts.map((fm) => (
-          <div key={fm.path}>{fm.title}</div>
-        ))}
+        <Feed posts={recentPosts} />
+        <Pagination
+          hasPrevPage={false}
+          hasNextPage={true}
+          prevPagePath={'/'}
+          nextPagePath={'/page/1'}
+        />
       </Page>
     </Layout>
   )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const recentPosts = (await getAllFrontMatters()).slice(
-    0,
-    DEFAULT_NUMBER_OF_POSTS,
-  )
+  const recentPosts = (await getAllPosts()).slice(0, DEFAULT_NUMBER_OF_POSTS)
 
   return {
     props: { recentPosts },
---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+import PAGINATION from '../../constants/pagination'
+
+const PaginationContainer = styled.div`
+  margin-top: 3.25rem;
+  display: flex;
+`
+
+const PaginationButton = styled.div<{ $direction: string }>`
+  width: 50%;
+  ${({ $direction }) => css`
+    text-align: ${$direction};
+  `}
+`
+
+const PaginationLink = styled.a<{ $disabled: boolean }>`
+  color: #f7a046;
+  font-size: 1.625rem;
+  font-weight: 700;
+
+  &::focus,
+  &::hover {
+    color: #5d93ff;
+  }
+
+  ${({ $disabled }) =>
+    $disabled
+      ? css`
+          pointer-events: none;
+          color: #bbb;
+        `
+      : null}
+`
+export default function Pagination({
+  prevPagePath,
+  nextPagePath,
+  hasNextPage,
+  hasPrevPage,
+}: {
+  prevPagePath: string
+  nextPagePath: string
+  hasNextPage: boolean
+  hasPrevPage: boolean
+}) {
+  console.log(hasPrevPage, hasNextPage)
+  return (
+    <PaginationContainer>
+      <PaginationButton $direction={'left'}>
+        <PaginationLink href={prevPagePath} $disabled={!hasPrevPage}>
+          {PAGINATION.PREV_PAGE}
+        </PaginationLink>
+      </PaginationButton>
+
+      <PaginationButton $direction={'right'}>
+        <PaginationLink href={nextPagePath} $disabled={!hasNextPage}>
+          {PAGINATION.NEXT_PAGE}
+        </PaginationLink>
+      </PaginationButton>
+    </PaginationContainer>
+  )
+}

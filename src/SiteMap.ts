77e4f942a01972b893ac49327552891847c624d4
yceffort <yceffort@gmail.com>
2020-10-15 15:52:26 +0900

    [#101] Sitemap, robots.txt

---
+++ src/SiteMap.ts
@@ -0,0 +1,40 @@
+import { writeFile } from 'promise-fs'
+
+import { getAllPosts } from './utils/Markdown'
+
+export async function createSiteMap() {
+  const posts = await getAllPosts()
+
+  const sitemap = `
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
+  xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
+  xmlns:xhtml="http://www.w3.org/1999/xhtml" 
+  xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"
+  xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+  xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+  <url>
+    <loc>${`https://yceffort.kr/about`}</loc>
+  </url>
+  <url>
+    <loc>${`https://yceffort.kr/contacts`}</loc>
+  </url>
+  <url>
+    <loc>${`https://yceffort.kr/tags`}</loc>
+  </url>
+    ${posts
+      .map(({ fields: { slug } }) => {
+        return `<url>
+                    <loc>${`https://yceffort.kr/${slug}`}</loc>
+                    <changefreq>daily</changefreq>
+                    <priority>0.7</priority>
+                </url>`
+      })
+      .join('\n')}
+</urlset>
+    `.replace(/^\s+|\r\n|\n|\r|(>)\s+(<)|\s+$/gm, '$1$2')
+
+  await writeFile('public/sitemap.xml', sitemap, { encoding: 'utf-8' })
+}
+
+createSiteMap()

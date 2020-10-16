c91fdfd5717d0fd5eee189ebc5855f3043770966
yceffort <yceffort@gmail.com>
2020-10-16 16:09:44 +0900

    [#110]  사이드바 기능 추가

--- src/components/Sidebar/Menu.tsx
+++ src/components/Sidebar/Menu.tsx
@@ -1,5 +1,6 @@
+import { useRouter } from 'next/router'
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import config from '../../config'
 
@@ -18,11 +19,19 @@ const MenuList = styled.ul`
   }
 `
 
-const MenuItem = styled.a`
+const MenuItem = styled.a<{ $active?: boolean }>`
   font-size: 1rem;
   color: #222;
   font-weight: 400;
   border: 0;
+
+  ${({ $active }) =>
+    $active
+      ? css`
+          color: #222;
+          border-bottom: 1px solid #222;
+        `
+      : null}
 
   &:hover,
   &:focus {
@@ -34,13 +43,23 @@ const MenuItem = styled.a`
 
 export default function Menu() {
   const { menu: menus } = config
+  const { pathname } = useRouter()
 
+  console.log(pathname)
   return (
     <MenuNav>
       <MenuList>
         {menus.map((menu, index) => (
           <li key={index}>
-            <MenuItem href={menu.path}>{menu.label}</MenuItem>
+            <MenuItem
+              href={menu.path}
+              $active={
+                menu.path === pathname ||
+                (menu.path === '/' && pathname.includes('page'))
+              }
+            >
+              {menu.label}
+            </MenuItem>
           </li>
         ))}
       </MenuList>

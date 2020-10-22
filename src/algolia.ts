c727c7c8d931afe71c5cb30ef915b299781bd810
yceffort <yceffort@gmail.com>
2020-10-22 17:08:35 +0900

    [#141] fix bug

--- src/algolia.ts
+++ src/algolia.ts
@@ -2,7 +2,7 @@ import algolia from 'algoliasearch'
 
 import { getAllPosts } from './utils/Markdown'
 
-const ALGOLIA_API_KEY = process.env.ALGOLIA_API_KEY
+const ALGOLIA_API_KEY = process.env.ALGOLIA_API_KEY || ''
 ;(async () => {
   try {
     const client = algolia('4KH5R593RQ', ALGOLIA_API_KEY)

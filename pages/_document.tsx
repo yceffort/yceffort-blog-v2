ed05c3f9bad719f5b7561f9cec89fc067a38f0da
yceffort <yceffort@gmail.com>
2020-11-23 13:06:11 +0900

    fix pwa in ios

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -119,11 +119,40 @@ export default class MyDocument extends Document<Props> {
           />
           <link rel="manifest" href="/manifest.json" />
           <meta name="msapplication-TileColor" content="#00b7ff" />
+          <meta name="application-name" content="yceffort" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="yceffort's blog" />
+          <meta name="description" content="yceffort's blog" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="msapplication-TileColor" content="#00b7ff" />
+          <meta name="msapplication-tap-highlight" content="no" />
           <meta
             name="msapplication-TileImage"
             content="/favicon/ms-icon-144x144.png"
           />
           <meta name="theme-color" content="#00b7ff" />
+
+          <link rel="apple-touch-icon" href="touch-icon-iphone.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="touch-icon-ipad.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="touch-icon-iphone-retina.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="167x167"
+            href="touch-icon-ipad-retina.png"
+          />
 
           <script
             async

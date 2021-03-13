f0bb80224a31e87fc26bfdcb143720bfb40060ae
yceffort <yceffort@gmail.com>
2021-03-13 10:57:29 +0900

    add naming-convention.js

---
+++ naming-convention.js
@@ -0,0 +1,30 @@
+// Edit your custom Exclude words
+const excludeWords = [
+  'cloud_name',
+  'api_key',
+  'api_secret',
+  'access_type',
+  'public_id',
+  'unique_filename',
+]
+const regex = `${excludeWords.join('|')})$`
+
+const defaultRule = require('eslint-config-yceffort/rules/typescript')
+
+const extendedRules = defaultRule.rules[
+  '@typescript-eslint/naming-convention'
+].map((item) =>
+  typeof item === 'string'
+    ? item
+    : {
+        ...item,
+        filter: {
+          regex: `${item.filter.regex.split(')$')[0]}|${regex}`,
+          match: false,
+        },
+      },
+)
+
+module.exports = {
+  extendedRules,
+}

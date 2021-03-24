6e9c352732d3bdcf9eff279d34940e10bbc9f45a
yceffort <yceffort@gmail.com>
2021-03-24 21:32:10 +0900

    fix wrong condition

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 const withPWA = require('next-pwa')
-const isProduction = process.env.NODE_ENV !== 'production'
+const isProduction = process.env.NODE_ENV === 'production'
 isProduction && require('dotenv').config()
 
 module.exports = withPWA({

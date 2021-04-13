2b78891a3a4edc5705ef7181701a015bcc8c6b7b
yceffort <yceffort@gmail.com>
2021-04-13 16:32:47 +0900

    about 페이지

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,33 +1,59 @@
 import React from 'react'
 
-import Layout from '#components/Layout'
-import Page from '#components/Page'
-import Sidebar from '#components/Sidebar/Sidebar'
-import config from '#src/config'
+import SiteConfig from '#src/config'
+import { PageSeo } from '#components/SEO'
+import SocialIcon from '#components/icons'
 
 export default function About() {
   return (
-    <Layout
-      title="about"
-      description={config.author.name}
-      url="https://yceffort.kr/about"
-    >
-      <Sidebar />
-      <Page title="About">
-        <div>
-          <p>Frontend Engineer in Korea.</p>
-          <p>Study Everyday, Dream Always.</p>
-          <p>
-            <a
-              href="https://www.notion.so/9fc4262c01744a63a849cdccdde5c85f"
-              target="_blank"
-              rel="nofollow noopener noreferrer"
-            >
-              Resume
-            </a>
-          </p>
+    <>
+      <PageSeo
+        title={`About - ${SiteConfig.author.name}`}
+        description={`About me - ${SiteConfig.author.name}`}
+        url={`${SiteConfig.url}/about`}
+      />
+      <div className="divide-y">
+        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            About
+          </h1>
         </div>
-      </Page>
-    </Layout>
+        <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
+          <div className="flex flex-col items-center pt-8 space-x-2">
+            {/* <img
+              src={siteMetadata.image}
+              alt="avatar"
+              className="w-48 h-48 rounded-full"
+            /> */}
+            <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">
+              {SiteConfig.author.name}
+            </h3>
+            <div className="text-gray-500 dark:text-gray-400">
+              Frontend Engineer
+            </div>
+            <div className="text-gray-500 dark:text-gray-400">Seoul, Korea</div>
+            <div className="flex pt-6 space-x-3">
+              <SocialIcon
+                kind="mail"
+                href={`mailto:${SiteConfig.author.contacts.email}`}
+              />
+              <SocialIcon
+                kind="github"
+                href={SiteConfig.author.contacts.github}
+              />
+              {/* <SocialIcon kind="facebook" href={siteMetadata.facebook} />
+              <SocialIcon kind="youtube" href={siteMetadata.youtube} />
+              <SocialIcon kind="linkedin" href={siteMetadata.linkedin} />
+              <SocialIcon kind="twitter" href={siteMetadata.twitter} /> */}
+            </div>
+          </div>
+          <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2">
+            <p>I am</p>
+            <p>Kim</p>
+            <p>yongchan</p>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }

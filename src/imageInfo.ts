e1b238a62fa0ce06dabad8101d2730fe1743e00b
yceffort <yceffort@gmail.com>
2021-05-01 22:46:16 +0900

    add image extension

--- src/imageInfo.ts
+++ src/imageInfo.ts
@@ -12,18 +12,23 @@ interface ImagesInterface {
 }
 
 async function getSizeOfImages() {
-  const result = ['png', '.jpg', '.jpeg'].reduce<ImagesInterface>(
-    (prev: ImagesInterface, extension: string) => {
-      const files = glob.sync(`public/**/*.${extension}`)
+  const result = [
+    'png',
+    'jpg',
+    'jpeg',
+    'gif',
+    'PNG',
+    'webp',
+    'svg',
+  ].reduce<ImagesInterface>((prev: ImagesInterface, extension: string) => {
+    const files = glob.sync(`public/**/*.${extension}`)
 
-      files.forEach((path) => {
-        const size = sizeOf(path)
-        prev[path] = size
-      })
-      return prev
-    },
-    {},
-  )
+    files.forEach((path) => {
+      const size = sizeOf(path)
+      prev[path] = size
+    })
+    return prev
+  }, {})
 
   await fs.promises.writeFile('public/imageInfo.json', JSON.stringify(result), {
     encoding: 'utf-8',

fc69a31e6a0d8b1db02aec360ac6cd2f3f520691
yceffort <yceffort@gmail.com>
2021-05-07 10:57:29 +0900

    Change text

--- pages/_offline.tsx
+++ pages/_offline.tsx
@@ -19,7 +19,7 @@ export default function Offline() {
         </p>
         <CustomLink href="/">
           <button className="inline px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg shadow focus:outline-none focus:shadow-outline-blue hover:bg-blue-700 dark:hover:bg-blue-500">
-            Back to homepage
+            Back to blog
           </button>
         </CustomLink>
       </div>

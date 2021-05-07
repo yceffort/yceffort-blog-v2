3bc8d3cf64990c0f5bfba75a2c3710a9399cd2d1
yceffort <yceffort@gmail.com>
2021-05-07 20:13:10 +0900

    lazy load katex

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -290,11 +290,17 @@ export default class MyDocument extends Document {
             <link
               rel="stylesheet"
               href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              onLoad="this.media='all'"
             />
           </noscript>
           <link
             rel="preload"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/fonts/KaTeX_Main-Regular.woff2"
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            onLoad="this.media='all'"
             as="font"
             type="font/woff2"
             crossOrigin="anonymous"
@@ -302,6 +308,9 @@ export default class MyDocument extends Document {
           <link
             rel="preload"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/fonts/KaTeX_Math-Italic.woff2"
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            onLoad="this.media='all'"
             as="font"
             type="font/woff2"
             crossOrigin="anonymous"
@@ -309,6 +318,9 @@ export default class MyDocument extends Document {
           <link
             rel="preload"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/fonts/KaTeX_Size2-Regular.woff2"
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            onLoad="this.media='all'"
             as="font"
             type="font/woff2"
             crossOrigin="anonymous"
@@ -316,6 +328,9 @@ export default class MyDocument extends Document {
           <link
             rel="preload"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/fonts/KaTeX_Size4-Regular.woff2"
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            onLoad="this.media='all'"
             as="font"
             type="font/woff2"
             crossOrigin="anonymous"
@@ -323,6 +338,9 @@ export default class MyDocument extends Document {
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/katex.min.css"
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            onLoad="this.media='all'"
             integrity="sha384-t5CR+zwDAROtph0PXGte6ia8heboACF9R5l/DiY+WZ3P2lxNgvJkQk5n7GPvLMYw"
             crossOrigin="anonymous"
           />

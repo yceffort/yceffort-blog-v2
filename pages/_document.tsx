5220f097ecfff2ec01cbe94b5b12ff997b2ab9a4
yceffort <yceffort@gmail.com>
2021-05-07 20:23:14 +0900

    add media print

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -281,23 +281,17 @@ export default class MyDocument extends Document {
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-            media="print"
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            onLoad="this.media='all'"
           />
           <noscript>
             <link
               rel="stylesheet"
               href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-              // @ts-ignore
-              onLoad="this.media='all'"
             />
           </noscript>
           <link
             rel="preload"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/fonts/KaTeX_Main-Regular.woff2"
+            media="print"
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             onLoad="this.media='all'"
@@ -308,6 +302,7 @@ export default class MyDocument extends Document {
           <link
             rel="preload"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/fonts/KaTeX_Math-Italic.woff2"
+            media="print"
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             onLoad="this.media='all'"
@@ -318,6 +313,7 @@ export default class MyDocument extends Document {
           <link
             rel="preload"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/fonts/KaTeX_Size2-Regular.woff2"
+            media="print"
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             onLoad="this.media='all'"
@@ -328,6 +324,7 @@ export default class MyDocument extends Document {
           <link
             rel="preload"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/fonts/KaTeX_Size4-Regular.woff2"
+            media="print"
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             onLoad="this.media='all'"
@@ -338,6 +335,7 @@ export default class MyDocument extends Document {
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/katex.min.css"
+            media="print"
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             onLoad="this.media='all'"

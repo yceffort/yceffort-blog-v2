0579393617521aa1c66f0bf2fea784fd3964b58a
yceffort <yceffort@gmail.com>
2021-05-25 23:57:38 +0900

    katex load 변경

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -291,10 +291,6 @@ export default class MyDocument extends Document {
           <link
             rel="preload"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/fonts/KaTeX_Main-Regular.woff2"
-            media="print"
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            onLoad="this.media='all'"
             as="font"
             type="font/woff2"
             crossOrigin="anonymous"
@@ -302,10 +298,6 @@ export default class MyDocument extends Document {
           <link
             rel="preload"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/fonts/KaTeX_Math-Italic.woff2"
-            media="print"
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            onLoad="this.media='all'"
             as="font"
             type="font/woff2"
             crossOrigin="anonymous"
@@ -313,10 +305,6 @@ export default class MyDocument extends Document {
           <link
             rel="preload"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/fonts/KaTeX_Size2-Regular.woff2"
-            media="print"
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            onLoad="this.media='all'"
             as="font"
             type="font/woff2"
             crossOrigin="anonymous"
@@ -324,21 +312,12 @@ export default class MyDocument extends Document {
           <link
             rel="preload"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/fonts/KaTeX_Size4-Regular.woff2"
-            media="print"
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            onLoad="this.media='all'"
-            as="font"
             type="font/woff2"
             crossOrigin="anonymous"
           />
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/katex.min.css"
-            media="print"
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            onLoad="this.media='all'"
             integrity="sha384-t5CR+zwDAROtph0PXGte6ia8heboACF9R5l/DiY+WZ3P2lxNgvJkQk5n7GPvLMYw"
             crossOrigin="anonymous"
           />

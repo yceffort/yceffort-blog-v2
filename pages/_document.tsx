afd3fa3eeba69c00f7231dd3021bd55f27408253
yceffort <yceffort@gmail.com>
2021-06-19 23:02:08 +0900

    [#343] remove unused imports

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default class MyDocument extends Document {
   render() {

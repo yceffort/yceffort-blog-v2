ab08a72738becbbc52375e470acec849a8b3a1b2
yceffort <yceffort@gmail.com>
2021-07-07 21:41:13 +0900

    [Bug] Fix sitemap

--- src/utils/siteMap.ts
+++ src/utils/siteMap.ts
@@ -15,9 +15,19 @@ async function createSiteMap() {
       `<url><loc>${`https://yceffort.kr/tags/${tag}`}</loc><changefreq>daily</changefreq><priority>0.3</priority></url>`,
   )
 
-  const sitemap = `<?xmlversion="1.0"encoding="UTF-8"?><urlsetxmlns="http://www.sitemaps.org/schemas/sitemap/0.9"xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"xmlns:xhtml="http://www.w3.org/1999/xhtml"xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"xmlns:video="http://www.google.com/schemas/sitemap-video/1.1"><url><loc>https://yceffort.kr/about</loc></url><url><loc>https://yceffort.kr/tags</loc></url>${postUrls.join(
-    '\n',
-  )}${tagUrls.join('\n')}</urlset>`
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
+    xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
+    xmlns:xhtml="http://www.w3.org/1999/xhtml" 
+    xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"
+    xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+    xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+    <url>
+      <loc>https://yceffort.kr/about</loc>
+    </url>    
+    <url>
+      <loc>https://yceffort.kr/tags</loc>
+    </url>${postUrls.join('\n')}${tagUrls.join('\n')}</urlset>`
 
   await fs.promises.writeFile('public/sitemap.xml', sitemap, {
     encoding: 'utf-8',

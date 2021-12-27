b2409db3d98a74330b40f8695c2c0292efeccd99
yceffort <yceffort@gmail.com>
2021-12-27 15:53:04 +0900

    Revert "build markdown"
    This reverts commit 9603b9792676ced7fe8a955e9bc882476903bea2.

--- src/utils/Markdown.ts
+++ src/utils/Markdown.ts
@@ -1,13 +1,33 @@
 import { serialize } from 'next-mdx-remote/serialize'
 import remarkMath from 'remark-math'
 import rehypeKatex from 'rehype-katex'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import prism from '@mapbox/rehype-prism'
 import toc from 'remark-toc'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import slug from 'remark-slug'
 import visit from 'unist-util-visit'
+import { Node } from 'unist'
 import sizeOf from 'image-size'
 
-const tokenClassNames = {
+import imageInfo from '../../public/imageInfo.json'
+
+type TokenType =
+  | 'tag'
+  | 'attr-name'
+  | 'attr-value'
+  | 'deleted'
+  | 'inserted'
+  | 'punctuation'
+  | 'keyword'
+  | 'string'
+  | 'function'
+  | 'boolean'
+  | 'comment'
+
+const tokenClassNames: { [key in TokenType]: string } = {
   tag: 'text-code-red',
   'attr-name': 'text-code-yellow',
   'attr-value': 'text-code-green',
@@ -23,26 +43,42 @@ const tokenClassNames = {
 
 const postPrefix = 'posts/'
 
-function getSizeOfImage(name) {
-  return sizeOf(`public${name}`)
+function getSizeOfImage(name: string) {
+  const path = `public${name}`
+  try {
+    if (process.env.NODE_ENV === 'production') {
+      return (imageInfo as any)[path]
+    } else {
+      return sizeOf(path)
+    }
+  } catch (e) {
+    console.error(`Error while get Size of image. path: ${name} error: ${e}`) // eslint-disable-line no-console
+    return {
+      height: undefined,
+      width: undefined,
+    }
+  }
 }
 
-export async function parseMarkdownToMdx(body, path) {
+export async function parseMarkdownToMdx(body: string, path: string) {
   return serialize(body, {
+    // scope: MDXComponents,
     mdxOptions: {
       remarkPlugins: [
         remarkMath,
         toc,
         slug,
         () => {
-          return (tree) => {
+          return (tree: Node) => {
             visit(
               tree,
-              (node) =>
+              (node: any) =>
                 node.type === 'paragraph' &&
-                node.children.some((n) => n.type === 'image'),
-              (node) => {
-                const imageNode = node.children.find((n) => n.type === 'image')
+                node.children.some((n: any) => n.type === 'image'),
+              (node: any) => {
+                const imageNode = node.children.find(
+                  (n: any) => n.type === 'image',
+                )
 
                 if (!imageNode.url.startsWith('http')) {
                   const startIndex = path.indexOf(postPrefix)
@@ -52,6 +88,7 @@ export async function parseMarkdownToMdx(body, path) {
                     endIndex,
                   )
 
+                  // /year/month/day 로 되어 있던 이미지 지원.
                   const tempImgSplit = tempImgPath.split('/')
                   const imgPath =
                     tempImgSplit.length > 2
@@ -87,8 +124,9 @@ export async function parseMarkdownToMdx(body, path) {
         prism,
         () => {
           return (tree) => {
-            visit(tree, 'element', (node) => {
-              const [token, type] = node.properties.className || []
+            visit(tree, 'element', (node: any) => {
+              const [token, type]: [string, TokenType] =
+                node.properties.className || []
               if (token === 'token') {
                 node.properties.className = [tokenClassNames[type]]
               }

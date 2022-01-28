5d590159dc633ac2046632b0af0379b4f498f2f1
yceffort <yceffort@gmail.com>
2022-01-28 14:58:48 +0900

    불필요한 주석 제거

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,10 +18,6 @@ export default function Footer() {
             href={SiteConfig.author.contacts.github}
             size={6}
           />
-          {/* <SocialIcon kind="facebook" href={siteMetadata.facebook} size="6" />
-          <SocialIcon kind="youtube" href={siteMetadata.youtube} size="6" />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
-          <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" /> */}
         </div>
         <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{SiteConfig.author.name}</div>

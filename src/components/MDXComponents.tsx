444af0826db79c6a83b47ebb8d8dd9d5eb23c0f7
yceffort <yceffort@gmail.com>
2022-03-19 00:47:21 +0900

    update mdx components

---
+++ src/components/MDXComponents.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import { HTMLProps } from 'react'
+
+import CustomLink from './Link'
+
+function NextImage(props: HTMLProps<HTMLImageElement>) {
+  return (
+    <Image
+      {...props}
+      crossOrigin="anonymous"
+      src={props.src || ''}
+      placeholder="blur"
+    />
+  )
+}
+
+const MdxComponents = {
+  img: NextImage,
+  a: CustomLink,
+}
+
+export default MdxComponents

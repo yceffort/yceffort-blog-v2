f574b568a30aa3c506ee974b161f63a7cf64779f
yceffort <yceffort@gmail.com>
2022-03-25 13:29:37 +0900

    fix custom.d.ts

--- typing/custom.d.ts
+++ typing/custom.d.ts
@@ -1,5 +1,4 @@
-/* eslint-disable  @typescript-eslint/no-explicit-any */
 declare module '*.svg' {
-  const content: any
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>
   export default content
 }

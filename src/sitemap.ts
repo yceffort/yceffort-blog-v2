cbfa9d9720e34d26d9b74e4dc3452296192094d9
yceffort <yceffort@gmail.com>
2023-05-23 00:17:11 +0900

    :sparkles: sitemap을 next 기능으로 마이그레이션

---
+++ src/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from 'next'
+
+import { getAllPosts, getAllTagsFromPosts } from '#utils/Post'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await getAllPosts()
+  const tags = await getAllTagsFromPosts()
+
+  return [
+    {
+      url: 'https://yceffort.kr',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://yceffort.kr/about',
+      lastModified: new Date(),
+    },
+    ...posts.map((post) => {
+      return {
+        url: `https://yceffort.kr/${post.fields.slug}`,
+        lastModified: new Date(post.frontMatter.date),
+      }
+    }),
+    ...tags.map((tag) => {
+      return {
+        url: `https://yceffort.kr/tags/${tag}`,
+      }
+    }),
+  ]
+}

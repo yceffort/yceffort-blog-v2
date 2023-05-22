9f0fcc166f3eab0b87ccf6b01594cf4143ef1b72
yceffort <yceffort@gmail.com>
2023-05-23 00:03:47 +0900

    :bug: suppressHydrationWarning

--- src/components/icons/themes/moon.tsx
+++ src/components/icons/themes/moon.tsx
@@ -6,7 +6,12 @@ export default function Moon() {
       fill="currentColor"
       className="text-gray-900 dark:text-gray-100"
     >
-      <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+      <path
+        /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
+        // @ts-ignore
+        suppressHydrationWarning
+        d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+      />
     </svg>
   )
 }

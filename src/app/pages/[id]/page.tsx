04e9eabfa457451599ed5f40d135883d68b266f8
yceffort <yceffort@gmail.com>
2023-05-23 00:06:50 +0900

    :sparkles: MathLoader 추가

--- src/app/pages/[id]/page.tsx
+++ src/app/pages/[id]/page.tsx
@@ -5,6 +5,7 @@ import { getAllPosts } from '#utils/Post'
 import { DEFAULT_NUMBER_OF_POSTS } from '#src/constants'
 import CustomLink from '#components/Link'
 import Tag from '#components/Tag'
+import MathLoader from '#components/layouts/Post/math'
 
 export async function generateMetadata({
   params: { id },
@@ -48,6 +49,7 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   return (
     <>
+      <MathLoader />
       <div className="divide-y">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">

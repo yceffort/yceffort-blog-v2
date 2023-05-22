532da61e889cad154972a32c0cf5574f1c683405
yceffort <yceffort@gmail.com>
2023-05-23 00:41:54 +0900

    :bug: mathloader 올바른 위치로 이동

--- src/app/[year]/[...slug]/page.tsx
+++ src/app/[year]/[...slug]/page.tsx
@@ -19,6 +19,7 @@ import PageTitle from '#components/PageTitle'
 import imageMetadata from '#utils/imageMetadata'
 import { parseCodeSnippet } from '#utils/Markdown'
 import MDXComponents from '#components/MDXComponents'
+import MathLoader from '#components/layouts/Post/math'
 
 export async function generateMetadata({
   params: { year, slug },
@@ -70,106 +71,109 @@ export default async function Page({
   const link = `https://github.com/yceffort/yceffort-blog-v2/issues/new?labels=%F0%9F%92%AC%20Discussion&title=[Discussion] issue on ${title}&assignees=yceffort&body=${SiteConfig.url}/${slug}`
 
   return (
-    <article>
-      <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
-        <header className="pt-6 xl:pb-6">
-          <div className="space-y-1 text-center">
-            <dl className="space-y-10">
-              <div>
-                <dt className="sr-only">Published on</dt>
-                <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                  <time dateTime={updatedAt}>{updatedAt}</time>
-                </dd>
-              </div>
-            </dl>
-            <div>
-              <PageTitle>{title}</PageTitle>
-            </div>
-          </div>
-        </header>
-        <div
-          className="divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0"
-          style={{ gridTemplateRows: 'auto 1fr' }}
-        >
-          <dl className="pt-6 pb-10 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
-            <dt className="sr-only">Author</dt>
-            <dd>
-              <ul className="flex justify-center space-x-8 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-8">
-                <li className="flex items-center space-x-2">
-                  <Image
-                    src={profile}
-                    placeholder="blur"
-                    alt="avatar"
-                    width={40}
-                    height={40}
-                    className="h-10 w-10 rounded-full"
-                  />
-                  <dl className="whitespace-nowrap text-sm font-medium leading-5">
-                    <dt className="sr-only">Name</dt>
-                    <dd className="text-gray-900 dark:text-gray-100">
-                      {SiteConfig.author.name}
-                    </dd>
-                  </dl>
-                </li>
-              </ul>
-            </dd>
-          </dl>
-          <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
-            <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">
-              {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-              {/* @ts-ignore */}
-              <MDXRemote
-                source={body}
-                components={MDXComponents}
-                options={{
-                  mdxOptions: {
-                    remarkPlugins: [
-                      remarkMath,
-                      remarkToc,
-                      remarkSlug,
-                      remarkGfm,
-                    ],
-                    rehypePlugins: [
-                      rehypeKatex,
-                      prism,
-                      parseCodeSnippet,
-                      rehypeAutolinkHeadings,
-                      imageMetadata(path),
-                    ],
-                  },
-                }}
-              />
-            </div>
-            <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
-              <CustomLink href={link}>Issue on GitHub</CustomLink>
-            </div>
-          </div>
-          <footer>
-            <div className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y">
-              {tags && (
-                <div className="py-4 xl:py-8">
-                  <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
-                    Tags
-                  </h2>
-                  <div className="flex flex-wrap">
-                    {tags.map((tag) => (
-                      <Tag key={tag} text={tag} />
-                    ))}
-                  </div>
+    <>
+      <MathLoader />
+      <article>
+        <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
+          <header className="pt-6 xl:pb-6">
+            <div className="space-y-1 text-center">
+              <dl className="space-y-10">
+                <div>
+                  <dt className="sr-only">Published on</dt>
+                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                    <time dateTime={updatedAt}>{updatedAt}</time>
+                  </dd>
                 </div>
-              )}
+              </dl>
+              <div>
+                <PageTitle>{title}</PageTitle>
+              </div>
             </div>
-            <div className="pt-4 xl:pt-8">
-              <CustomLink
-                href="/"
-                className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
-              >
-                &larr; Back to the blog
-              </CustomLink>
+          </header>
+          <div
+            className="divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0"
+            style={{ gridTemplateRows: 'auto 1fr' }}
+          >
+            <dl className="pt-6 pb-10 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
+              <dt className="sr-only">Author</dt>
+              <dd>
+                <ul className="flex justify-center space-x-8 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-8">
+                  <li className="flex items-center space-x-2">
+                    <Image
+                      src={profile}
+                      placeholder="blur"
+                      alt="avatar"
+                      width={40}
+                      height={40}
+                      className="h-10 w-10 rounded-full"
+                    />
+                    <dl className="whitespace-nowrap text-sm font-medium leading-5">
+                      <dt className="sr-only">Name</dt>
+                      <dd className="text-gray-900 dark:text-gray-100">
+                        {SiteConfig.author.name}
+                      </dd>
+                    </dl>
+                  </li>
+                </ul>
+              </dd>
+            </dl>
+            <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
+              <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">
+                {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+                {/* @ts-ignore */}
+                <MDXRemote
+                  source={body}
+                  components={MDXComponents}
+                  options={{
+                    mdxOptions: {
+                      remarkPlugins: [
+                        remarkMath,
+                        remarkToc,
+                        remarkSlug,
+                        remarkGfm,
+                      ],
+                      rehypePlugins: [
+                        rehypeKatex,
+                        prism,
+                        parseCodeSnippet,
+                        rehypeAutolinkHeadings,
+                        imageMetadata(path),
+                      ],
+                    },
+                  }}
+                />
+              </div>
+              <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
+                <CustomLink href={link}>Issue on GitHub</CustomLink>
+              </div>
             </div>
-          </footer>
+            <footer>
+              <div className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y">
+                {tags && (
+                  <div className="py-4 xl:py-8">
+                    <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                      Tags
+                    </h2>
+                    <div className="flex flex-wrap">
+                      {tags.map((tag) => (
+                        <Tag key={tag} text={tag} />
+                      ))}
+                    </div>
+                  </div>
+                )}
+              </div>
+              <div className="pt-4 xl:pt-8">
+                <CustomLink
+                  href="/"
+                  className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
+                >
+                  &larr; Back to the blog
+                </CustomLink>
+              </div>
+            </footer>
+          </div>
         </div>
-      </div>
-    </article>
+      </article>
+    </>
   )
 }

98d0b86ce674f9217ea73d5686512bc0e035ea27
yceffort <yceffort@gmail.com>
2023-05-23 22:15:19 +0900

    :rocket: add robots.ts

---
+++ src/app/robot.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://yceffort.kr/sitemap.xml',
+  }
+}
